462e970a1b7dbc0208f482ba91adc9155d27887b
Vishnu <nandam.vishnutejaa@gmail.com>
2024-03-19 08:34:26 -0700

    Initial Commit

---
+++ Cities/CitiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitiesApp.swift
+//  Cities
+//
+//  Created by Vishnu Tejaa on 3/19/24.
+//
+
+import SwiftUI
+
+@main
+struct CitiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
